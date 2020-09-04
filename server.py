16ba5f1b531bce3637aa6c99b1a218e7d84c9cc7
carlinhosmugiwara <55931229+carlinhosmugiwara@users.noreply.github.com>
2020-09-04 18:49:20 -0300

    Update server.py

--- server.py
+++ server.py
@@ -3,7 +3,7 @@ import threading # library to create threads
 
 # defining host and port
 host_ip = '127.0.0.1' # local machine ip here, but if you're in a domain or something like that put the ip here
-port = 5555 # this is a port that uses tcp and this is why it was chosen, tcp makes possible for hosts to exchange packets of data
+port = 5555 # this is a port that uses tcp and this is why it was chosen, tcp makes possible for users and host to exchange packets of data
 
 # defining the server socket
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # AF_INET means that we're creating a internet socket that is ipv4, SOCK_STREAM means that is a tcp socket
